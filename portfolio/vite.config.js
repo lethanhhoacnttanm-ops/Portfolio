5b0f65883e6cb00906c47311d22ae81ec7fa8261
lethanhhoa <lethanhhoa.cntt.anm@gmail.com>
2026-04-13 18:52:18 +0700

    new upd

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -33,4 +33,17 @@ export default defineConfig(({ mode }) => ({
     include: ["src/**/*.{test,spec}.{js,jsx}"], 
     teardownTimeout: 1000,
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, 
+  },
 }));
